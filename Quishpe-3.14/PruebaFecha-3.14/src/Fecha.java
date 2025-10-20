2194016dc36126df30ead3084d37f369f68d9611
SebasMinda <sebastian.minda@udla.edu.ec>
2025-10-19 20:15:56 -0500

    Cambios

--- Quishpe-3.14/PruebaFecha-3.14/src/Fecha.java
+++ Quishpe-3.14/PruebaFecha-3.14/src/Fecha.java
@@ -1,16 +1,16 @@
 /**
  * @author Isaac Quishpe
- * @deprecated
- * 3.14(La clase Fecha) Cree una clase llamada Fecha, que incluya tres variables de instancia: un mes (tipo int), un día
- *(tipo int) y un año (tipo int). Su clase debe tener un constructor que inicialice las tres variables de instancia,
- *y debe asumir que los valores que se proporcionan son correctos.
- *Proporcione un método establecer y un método obtener para cada variable de instancia. Proporcione un método mostrarFecha,
- *que muestre el mes, día y año, separados por barras diagonales (/).
- *Escriba una aplicación de prueba llamada PruebaFecha, que demuestre las capacidades de la clase Fecha.
+ * @deprecated 3.14(La clase Fecha) Cree una clase llamada Fecha, que incluya tres variables de instancia: un mes (tipo int), un día
+ * (tipo int) y un año (tipo int). Su clase debe tener un constructor que inicialice las tres variables de instancia,
+ * y debe asumir que los valores que se proporcionan son correctos.
+ * Proporcione un método establecer y un método obtener para cada variable de instancia. Proporcione un método mostrarFecha,
+ * que muestre el mes, día y año, separados por barras diagonales (/).
+ * Escriba una aplicación de prueba llamada PruebaFecha, que demuestre las capacidades de la clase Fecha.
  *
  */
 
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Fecha {
@@ -18,7 +18,6 @@ public class Fecha {
     private int mes;
     private int dia;
     private int anio;
-
 
 
     // constructor
@@ -29,6 +28,7 @@ public class Fecha {
 
 
     }
+
     //getters and setters
     public int getMes() {
         return mes;
@@ -54,10 +54,18 @@ public class Fecha {
         this.anio = anio;
     }
 
-
+    /** Por cumplir con el requisito de usar ventanas emergentes cambiamos por JOption pane*/
     /* metodo para mostrar datos*/
-    public void mostrarDatos(){
+    // public void mostrarDatos(){
 
-        System.out.print(dia  +" /" + mes + "/" + anio );
+    //      System.out.print(dia  +" /" + mes + "/" + anio );
+    //  }
+
+    /**
+     * Metodo modificado con JOption pane para mostrar la fecha
+     */
+    public void mostrarDatos() {
+        JOptionPane.showMessageDialog(null, "****Fecha ingresada****\n"+dia + " / " + mes + " / " + anio);
     }
+
 }
--- Quishpe-3.14/PruebaFecha-3.14/src/Main.java
+++ Quishpe-3.14/PruebaFecha-3.14/src/Main.java
@@ -1,42 +1,58 @@
+import javax.swing.*;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
         int dia;
         int mes;
         int anio;
+/** Por cumplir con el requisito de usar ventanas emergentes cambiamos e println y printf por JOption pane*/
+/**
+ Scanner sc= new Scanner(System.in);
+
+ do{
+ System.out.println("Ingrese el dia: ");
+ dia= sc.nextInt();
 
 
-        Scanner sc= new Scanner(System.in);
+ }while(dia <= 0 || dia >= 32);;
 
-        do{
-            System.out.println("Ingrese el dia: ");
-            dia= sc.nextInt();
+ do {
 
 
-        }while(dia <= 0 || dia >= 32);;
+ System.out.println("Ingrese el mes: ");
+ mes = sc.nextInt();
+ }while(mes <= 0 || mes >= 13);;
+
+ do {
+
+
+ System.out.println("Ingrese el anio");
+ anio = sc.nextInt();
+ }while(anio <=2000 || anio >= 2026);;
+
+ Fecha fecha1 = new Fecha(dia, mes, anio);
+
+ System.out.println("****Fecha ingresada****");
+ fecha1.mostrarDatos();
+ */
+/** Usando JOptionPane para entrada y salida de datos */
+        do {
+            String diaStr = JOptionPane.showInputDialog(null, "Ingrese el día (1-31):");
+            dia = Integer.parseInt(diaStr);
+        } while (dia <= 0 || dia > 31);
 
         do {
-
-
-            System.out.println("Ingrese el mes: ");
-            mes = sc.nextInt();
-        }while(mes <= 0 || mes >= 13);;
+            String mesStr = JOptionPane.showInputDialog(null, "Ingrese el mes (1-12):");
+            mes = Integer.parseInt(mesStr);
+        } while (mes <= 0 || mes > 12);
 
         do {
-
-
-            System.out.println("Ingrese el anio");
-            anio = sc.nextInt();
-        }while(anio <=2000 || anio >= 2026);;
-
-        Fecha fecha1 = new Fecha(dia, mes, anio);
-
-        System.out.println("****Fecha ingresada****");
+            String anioStr = JOptionPane.showInputDialog(null, "Ingrese el año (ej: 2025):");
+            anio = Integer.parseInt(anioStr);
+        } while (anio <= 0);
+        Fecha fecha1 = new Fecha(mes, dia, anio);
         fecha1.mostrarDatos();
-
     }
 }
